a907799d947b14d4c82f1c9e0e44149130ad0a54
mcmah309 <dillonmcmahon79@gmail.com>
2024-03-04 14:49:50 +0000

    doc: prelude

--- lib/prelude.dart
+++ lib/prelude.dart
@@ -1,14 +1,13 @@
-/// Rust comes with a variety of things in its standard library.
+/// rust_std comes with a variety of things in its standard library.
 /// However, if you had to manually import every single thing that you used,
 /// it would be very verbose. But importing a lot of things that a program never uses isn’t good either.
 /// A balance needs to be struck.
-/// The prelude is the list of things that Rust automatically imports into every Rust program. 
-/// It’s kept as small as possible, and is focused on things, particularly traits, 
-/// which are used in almost every single Rust program.
+/// The prelude is the list of things that rust_std should automatically imports into almost all projects. 
+/// It’s kept as small as possible, and is focused on things, 
+/// which are used in almost every single rust_std program.
 library prelude;
 
-export 'cell.dart';
-export 'collections.dart' show Vec; //todo when it makes sense, break up collections into the right modules
+export 'vec.dart';
 export 'iter.dart';
 export 'option.dart';
 export 'panic.dart';
