403f0c2280a200d9ad7eedd4bf99f21000bbad99
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-03 16:45:33 +0000

    test: Update

--- test/rust_std_test.dart
+++ test/rust_std_test.dart
@@ -2,7 +2,7 @@ import 'package:rust_std/result.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("test", () {
+  test("rust_std", () {
     // ignore: unused_local_variable
     Result<int, int> x = Ok(1);
   });
